dd597e04e7a8b3c4a825c483765be2174e9517f9
Avijit Pandey <avijeetpandey87@gmail.com>
2021-02-06 22:53:06 +0530

    *refactored code \n*added error-loggin

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const auth = require('../middlewares/auth');  //here auth is authorization not a
 const uploadAvatar = require('../middlewares/uploadAvatar');
 const bcrypt = require('bcryptjs');
 const _ = require('lodash');
-const {User, validateUser, validateEditUser, pickUserData} = require('../models/user');
+const { User, validateUser, validateEditUser, pickUserData } = require('../models/user');
 const express = require('express');
 const router = express.Router();
 
@@ -22,15 +22,10 @@ router.post('/add', async (req, res) => {
     let user = await User.findOne({ email: req.body.email });
     if (user) return res.status(400).send('User already registered.');
 
-    try {
-        user = new User(pickUserData(req.body));
+    user = new User(pickUserData(req.body));
     const salt = await bcrypt.genSalt(10);
     user.password = await bcrypt.hash(user.password, salt);
     await user.save();
-    } catch (error) {
-        console.error(error);
-    }
-    
     res.status(200).send(_.pick(user, ['_id', 'name', 'email']));
 });
 
@@ -42,15 +37,15 @@ router.put('/edit', [auth, uploadAvatar.single('avatar')], async (req, res) => {
     else req.body.avatar = req.user.avatar;
 
     const { error } = validateEditUser(req.body);
-    if(error) return res.status(400).send(error.details[0].message);
+    if (error) return res.status(400).send(error.details[0].message);
 
-    const result = await User.updateOne({_id: req.user._id}, {
+    const result = await User.updateOne({ _id: req.user._id }, {
         $set: {
             name: req.body.name,
             avatar: req.body.avatar
         }
     });
-    if(result.n) res.status(200).send('Your profile has been successfully updated. ');
+    if (result.n) res.status(200).send('Your profile has been successfully updated. ');
     else res.status(500).send("Error! please, try again later...");
 })
 
