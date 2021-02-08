cfa40d8190a371d97b279e1d7356cb2552046956
AndroAvi <avijeetpandey87@gmail.com>
2021-02-08 20:31:56 +0530

    added password complexity and added alternative hotfix for the user avatar link issue.

--- middlewares/uploadAvatar.js
+++ middlewares/uploadAvatar.js
@@ -9,10 +9,11 @@ const storage = multer.diskStorage({
     }
 });
 
-//TODO: file filter not working...
+
 const fileFilter = (req, file, cb) => {
     const allowedFileTypes = ['image/jpg', 'image/jpeg', 'image/png'];
     if(allowedFileTypes.includes(file.mimetype)){
+        file.db_url = `${process.env.BASE_URL}avatars/${file.originalname}`;
         cb(null, true);
     } else {
         cb(new Error('only files of type jpeg, jpg and png are allowed.'), false);
