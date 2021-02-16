734d181958d09ffbd8f65b3e860d442cf2cc296c
Ayushman <51037059+Dark0017@users.noreply.github.com>
2021-02-16 16:58:50 +0530

    Complete Branch model

--- models/branch.js
+++ models/branch.js
@@ -1,5 +1,7 @@
 const Joi = require("joi");
+Joi.objectId = require("joi-objectid")(Joi);
 const mongoose = require("mongoose");
+const _ = require("lodash")
 
 const branchSchema = mongoose.Schema({
   branch: {
@@ -16,3 +18,23 @@ const branchSchema = mongoose.Schema({
       ref: "Course"
   }]
 });
+
+const Branch = mongoose.model("Branch", branchSchema);
+
+function validate(branch){
+  const schema = Joi.object({
+    branch: Joi.string().required().min(3).max(255),
+    semester: Joi.string.min(1).max(8),
+    courses: Joi.array().items(Joi.objectId());
+  });
+}
+
+function pickData(branch){
+  return _.pick(branch, [
+    "branch", "sememster", "cousrses" 
+  ]);
+}
+
+exposts.Branch = Branch;
+exports.validateBranch = validate;
+exports.pickBranchData = pickData;
---
+++ routes/branches.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const {Branch, validateBranch}
+
+const route = express.Router();
+
+
+
+//create
+
+route.post("/add", async (req, res) => {
+
+});
+
+//retrive
+//update
+//delete
