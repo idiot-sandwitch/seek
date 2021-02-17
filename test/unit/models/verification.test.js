55f63e621e88cf07f1b4b2f7b7998a31446180db
AndroAvi <avijeetpandey87@gmail.com>
2021-02-17 19:58:33 +0530

    reformatted verification test code

--- test/unit/models/verification.test.js
+++ test/unit/models/verification.test.js
@@ -3,26 +3,36 @@ const { validateVerification } = require("../../../models/verification");
 const expect = require("chai").expect;
 
 describe("verification model", () => {
-  it("should return an error if no token is passed.", () => {
-    const { error } = validateVerification({});
-    expect(error).to.not.be.undefined;
-  });
-  it("should return an error if empty token is passed.", () => {
-    const { error } = validateVerification({ token: "" });
-    expect(error).to.not.be.undefined;
-  });
-  it("should return an error if invalid token is passed.", () => {
-    const { error } = validateVerification({ token: "1234" });
-    expect(error).to.not.be.undefined;
-  });
-  it("should return an error if extra arguments are passed.", () => {
-    const { error } = validateVerification({ token: "", arg1: "2" });
-    expect(error).to.not.be.undefined;
-  });
-  it("should return no errors if valid token is passed.", () => {
-    const { error } = validateVerification({
-      token: randomString.generate(128),
+  describe("validate", () => {
+    it("should return an error if no token is passed.", () => {
+      const { error } = validateVerification({});
+      expect(error).to.not.be.undefined;
     });
-    expect(error).to.be.undefined;
+    it("should return an error if empty token is passed.", () => {
+      const { error } = validateVerification({ token: "" });
+      expect(error).to.not.be.undefined;
+    });
+    it("should return an error if token length is less than 128 chars.", () => {
+      const { error } = validateVerification({ token: "1234" });
+      expect(error).to.not.be.undefined;
+    });
+    it("should return an error if token length is more than 128 chars.", () => {
+      const { error } = validateVerification({ token: "1".repeat(129) });
+      expect(error).to.not.be.undefined;
+    });
+    it("should return an error if token type is invalid.", () => {
+      const { error } = validateVerification({ token: 1234 });
+      expect(error).to.not.be.undefined;
+    });
+    it("should return an error if extra arguments are passed.", () => {
+      const { error } = validateVerification({ token: "", arg1: "2" });
+      expect(error).to.not.be.undefined;
+    });
+    it("should return no errors if valid token is passed.", () => {
+      const { error } = validateVerification({
+        token: randomString.generate(128),
+      });
+      expect(error).to.be.undefined;
+    });
   });
 });
